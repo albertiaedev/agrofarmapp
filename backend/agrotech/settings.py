bddd28a356fca76665707fcdad57ceb32a53f2d6
albertiaedev <albertiae.digital@gmail.com>
2024-11-14 12:44:01 -0400

    UPDATE: Include custom apps in settings.py

--- backend/agrotech/settings.py
+++ backend/agrotech/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #CUSTOM APPS
+    'aves_poultry',
+    'bovinos_cattle',
+    'cerdos_swine',
+    'equinos_equine',
 ]
 
 MIDDLEWARE = [
