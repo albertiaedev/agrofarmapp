1b0f747f50932cf5dffd5977c71c8dc55e753eb3
albertiaedev <albertiae.digital@gmail.com>
2024-12-21 17:22:40 -0400

    Update: Configure CORS in settings.py

--- backend/agrotech/settings.py
+++ backend/agrotech/settings.py
@@ -57,6 +57,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'agrotech.urls'
