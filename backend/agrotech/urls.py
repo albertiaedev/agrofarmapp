5a8e15a6f017aa92c065aa883ed641eb3f32cded
albertiaedev <albertiae.digital@gmail.com>
2025-02-13 17:07:55 -0400

    Update: Create urls for the api views

--- backend/agrotech/urls.py
+++ backend/agrotech/urls.py
@@ -1,29 +1,16 @@
-"""
-URL configuration for agrotech project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from aves_poultry.api_views import PoultryListView
 
 urlpatterns = [
 
     # ADMIN PANNEL SUPERUSER
     path('admin/', admin.site.urls),
-    '''
-    Username: albertiaedev
-    Password: agrotech
-    '''
+    # Username: albertiaedev
+    # Password: agrotech
     
+    # API VIEWS
+    path('api/poultry/', PoultryListView.as_view(), name='poultry-list'),
+    path('', include('aves_poultry.urls')),
+
 ]
